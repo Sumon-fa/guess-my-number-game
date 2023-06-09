b68fe37ebd87050b051a054cb7af5cc589991729
Sumon Fakir <fakirsumon78@gmail.com>
2023-06-09 10:37:04 +0300

    added startgame screen

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,15 +1,49 @@
-import {Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 
 interface PrimaryButtonProps {
   children: React.ReactNode;
 }
 
 const PrimaryButton = ({children}: PrimaryButtonProps) => {
+  function pressHandler() {
+    console.warn(45);
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{color: '#640233'}}
+        style={({pressed}) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
