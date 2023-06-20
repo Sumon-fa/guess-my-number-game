d906b07e794831c9ce2bc9c58741af958a947d45
Sumon Fakir <fakirsumon78@gmail.com>
2023-06-20 16:14:20 +0300

    added foreground image

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import Title from '../components/Title';
+import colors from '../constants/colors';
 
 const GameOverScreen = () => {
   return (
-    <View>
-      <Text>Game is Over</Text>
+    <View style={styles.rootContainer}>
+      <Title>Game Over !</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/image/success.png')}
+        />
+      </View>
     </View>
   );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: colors.primary800,
+    margin: 36,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
